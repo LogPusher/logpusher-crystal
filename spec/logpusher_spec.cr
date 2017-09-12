012127adc79e7a2dac4a0ea4f4de27d494558001
İzni Burak Demirtaş <izniburak@outlook.com>
2017-09-12 06:57:33 +0300

    Updated specs.

--- spec/logpusher_spec.cr
+++ spec/logpusher_spec.cr
@@ -1,20 +1,15 @@
-require "json"
 require "./spec_helper"
 
 describe LogPusher do
-  client = LogPusher::Client.new("hello@logpusher.com", "PASSWORD", "API_KEY")
+  it "valid email" do
+    email = LogPusher::Email.new("hello@logpusher.com")
+    email.get.should eq("hello@logpusher.com")
+  end
 
-  response = client.push(
-    "My awesome log message",
-    "myawesomesite.com",
-    "E-commerce",
-    "Notice",
-    Time.new.to_s("%H:%M"),
-    Time.new,
-    "1"
-  )
-
-  it "send push" do
-    response.status_code.should eq(200)
+  it "generate auth key" do
+    auth_key = LogPusher::AuthKey.new(
+      LogPusher::Email.new("hello@logpusher.com"), "123456"
+    )
+    auth_key.get_auth_key.is_a?(String).should eq(true)
   end
 end
